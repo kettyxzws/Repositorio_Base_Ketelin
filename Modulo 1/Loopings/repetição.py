da003be094a7c711ca40e13f72a1875cf68a8bdb
kettyxzws <ketelinvitoriq@gmail.com>
2025-09-16 11:05:30 -0300

    Create repetição.py

---
+++ Modulo 1/Loopings/repetição.py
@@ -0,0 +1,17 @@
+opcao =4
+while opcao !=3:
+    opcao=input("o que deseja fazer : \n1 - converter REAL para DOLAR \n2 - converter DOLAR para REAL \n3 - SAIR")
+    if == opcao 1:
+    def converter_real_para_dolar():
+        cotacao= 5.30
+        real=float(input("quanto vc tem em real?"))
+        converter= real* cotacao
+        print(converter)
+    converter_real_para_dolar()
+    else:
+    def converter_dolar_para_real():
+        cotacao=0.18
+        dolar=float(input("quanto vc tem em dolar?"))
+        converter=dolar * cotacao
+        print(converter)
+    converter_dolar_para_real()
