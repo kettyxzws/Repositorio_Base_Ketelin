b597472da9b5d4171278bdbeae1ff7cb1f856cfe
kettyxzws <ketelinvitoriq@gmail.com>
2025-09-16 09:03:33 -0300

    Create tabuada.py

---
+++ Modulo 1/Loopings/tabuada.py
@@ -0,0 +1,6 @@
+numero =  int(input(f"Digite o numero:"))
+numero_inicio = int(input(f" Digite o numero inicio:"))
+numero_fim = int(input(f"Digite o numero para terminar:"))
+
+for i in range(numero_inicio,numero_fim +18):
+         print(f" {i} x {numero} = {i * numero}")
