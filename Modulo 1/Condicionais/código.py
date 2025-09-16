2c41ecc1ae20071c56456a59dbd36c7928d65568
kettyxzws <ketelinvitoriq@gmail.com>
2025-09-16 09:29:38 -0300

    Create código.py

---
+++ Modulo 1/Condicionais/código.py
@@ -0,0 +1,14 @@
+opção = int(input("Digite o que deseja fazer: \n1- Fazer o Pix \n2-Extrato\n3-Depositar\n4-Sair"))
+if opção == 1 :
+    valor = input("Digite o valor a mandar : ")
+    quem = input("Digite para quem eu vou mandar (Ketelin ")
+    print("Pix realizado com sucesso ! ! 🤑 ")
+
+elif opção ==2:
+    p2 = input("Me diga o seu extrato : ")
+
+elif opção ==3:
+    p3 = input("Quantos voce vai depositar? ")
+
+else:
+    print("pronto,flw ! ! 👌")
